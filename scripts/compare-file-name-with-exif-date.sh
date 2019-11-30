6eeb610f8ae6b2744fca09219272c37e6fa427e0
Hendri Pretorius <pretorh@gmail.com>
2019-11-30 15:04:42 +0200

    compare exif with filename script changes
    - ignore failed to read (empty string)
    - get only a substring of the filename's digits
    - convert to bash script

--- scripts/compare-file-name-with-exif-date.sh
+++ scripts/compare-file-name-with-exif-date.sh
@@ -1,10 +1,16 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
 exif_date=$(exiv2 "$1" | grep timestamp | awk -F ' : ' '{print $2}' | awk -F ':' '{print $1"-"$2"-"$3"."$4"."$5}')
+# ignore failed to read
 [ -z "$exif_date" ] && exit 0
+[ "$exif_date" = "--.." ] && exit 0
 
-date_simple=$(echo "$exif_date" | sed 's|[^0-9]||g')
-filename_simple=$(basename "$1" | sed 's|[^0-9]||g')
+# get only the digits in the exif date
+date_simple=${exif_date//[^0-9]/}
+
+# get only the digits in the filename, and only the first 14 (to ignore enumarations)
+filename_digits=$(basename "$1" | sed 's|[^0-9]||g')
+filename_simple=${filename_digits:0:14}
 
 if [ "$date_simple" != "$filename_simple" ] ; then
     echo "$1 ($exif_date)"
